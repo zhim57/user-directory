29ae6e2001e428a5054a7e78ae3e5710102dbe5c
zhim57 <65269162+zhim57@users.noreply.github.com>
2020-10-27 00:38:34 -0400

    new docs

---
+++ docs/precache-manifest.be27152a788af00ed818521d0933bfe8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae5cd3169370bba141d240dcf7b29d4",
+    "url": "/user-directory/index.html"
+  },
+  {
+    "revision": "c22abb11ad96ccdfeb4f",
+    "url": "/user-directory/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "ca917ebba89db2d117bd",
+    "url": "/user-directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c22abb11ad96ccdfeb4f",
+    "url": "/user-directory/static/js/2.317108fe.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/user-directory/static/js/2.317108fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca917ebba89db2d117bd",
+    "url": "/user-directory/static/js/main.61bf5339.chunk.js"
+  },
+  {
+    "revision": "fa22a42c8028519eec71",
+    "url": "/user-directory/static/js/runtime-main.e0731c13.js"
+  }
+]);
